d5d73e711809d5e6dccfeb9b8b594c24ccdebca0
Igor Pieruccini <igorpieruccini@gmail.com>
2024-08-08 08:32:29 +0200

    Adding methods to calculating food spawn plus unit test

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,65 +1,77 @@
-import { createSections } from "./utils";
+import { Palco2D } from "palco-2d";
+import { BodyType, SnakeBodyType } from "../types";
+import { IsSectionAvailable, getRandomSection } from "./utils";
+import { Sprite } from "palco-2d/src/core/Sprite";
 
 describe('Utils', () => {
-  it('should return the correct value', () => {
-    const sections = createSections(9, 9);
-    expect(sections).toEqual(
-      [
-        [
-          // top left
-          { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 },
-          { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 },
-          { x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }
-        ],
-        [
-          // top middle
-          { x: 0, y: 3 }, { x: 0, y: 4 }, { x: 0, y: 5 },
-          { x: 1, y: 3 }, { x: 1, y: 4 }, { x: 1, y: 5 },
-          { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 2, y: 5 }
-        ],
-        [
-          // top right
-          { x: 0, y: 6 }, { x: 0, y: 7 }, { x: 0, y: 8 },
-          { x: 1, y: 6 }, { x: 1, y: 7 }, { x: 1, y: 8 },
-          { x: 2, y: 6 }, { x: 2, y: 7 }, { x: 2, y: 8 }
-        ],
-        [
-          // middle left
-          { x: 3, y: 0 }, { x: 3, y: 1 }, { x: 3, y: 2 },
-          { x: 4, y: 0 }, { x: 4, y: 1 }, { x: 4, y: 2 },
-          { x: 5, y: 0 }, { x: 5, y: 1 }, { x: 5, y: 2 }
-        ],
-        [
-          // center
-          { x: 3, y: 3 }, { x: 3, y: 4 }, { x: 3, y: 5 },
-          { x: 4, y: 3 }, { x: 4, y: 4 }, { x: 4, y: 5 },
-          { x: 5, y: 3 }, { x: 5, y: 4 }, { x: 5, y: 5 }
-        ],
-        [
-          // middle right
-          { x: 3, y: 6 }, { x: 3, y: 7 }, { x: 3, y: 8 },
-          { x: 4, y: 6 }, { x: 4, y: 7 }, { x: 4, y: 8 },
-          { x: 5, y: 6 }, { x: 5, y: 7 }, { x: 5, y: 8 }
-        ],
-        [
-          // bottom left
-          { x: 6, y: 0 }, { x: 6, y: 1 }, { x: 6, y: 2 },
-          { x: 7, y: 0 }, { x: 7, y: 1 }, { x: 7, y: 2 },
-          { x: 8, y: 0 }, { x: 8, y: 1 }, { x: 8, y: 2 }
-        ],
-        [
-          // bottom middle
-          { x: 6, y: 3 }, { x: 6, y: 4 }, { x: 6, y: 5 },
-          { x: 7, y: 3 }, { x: 7, y: 4 }, { x: 7, y: 5 },
-          { x: 8, y: 3 }, { x: 8, y: 4 }, { x: 8, y: 5 }
-        ],
-        [
-          // bottom right
-          { x: 6, y: 6 }, { x: 6, y: 7 }, { x: 6, y: 8 },
-          { x: 7, y: 6 }, { x: 7, y: 7 }, { x: 7, y: 8 },
-          { x: 8, y: 6 }, { x: 8, y: 7 }, { x: 8, y: 8 }
-        ]
-      ],
-    );
+
+  it("get random section", () => {
+
+    let result = { x: 0, y: 0 };
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0);
+    result = getRandomSection({ x: 0, y: 0 }, 4);
+    expect(result).toEqual({ x: 1, y: 1 });
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(1);
+    result = getRandomSection({ x: 0, y: 0 }, 4);
+    expect(result).toEqual({ x: 3, y: 3 });
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+    result = getRandomSection({ x: 0, y: 0 }, 4);
+    expect(result).toEqual({ x: 2, y: 2 });
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0);
+    result = getRandomSection({ x: 3, y: 3 }, 4);
+    expect(result).toEqual({ x: 2, y: 2 });
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(1);
+    result = getRandomSection({ x: 3, y: 3 }, 4);
+    expect(result).toEqual({ x: 0, y: 0 });
+
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+    result = getRandomSection({ x: 3, y: 3 }, 4);
+    expect(result).toEqual({ x: 1, y: 1 });
   });
+
+  describe("is section available", () => {
+
+    // beforeAll(() => {
+    //   const windowSpy = jest.spyOn(window, "window", "get");
+    //   // @ts-expect-error - Don't need to mock all properties only the one that is being used
+    //   windowSpy.mockImplementation(() => ({ innerWidth: 800, innerHeight: 600, devicePixelRatio: 1 }));
+    // });
+    //
+    // afterAll(() => {
+    //   jest.restoreAllMocks();
+    // });
+
+    it("Should return true since the snake is not on every spot of the section", () => {
+      const sprite = new Palco2D.BaseEntity({
+        position: { x: 0, y: 0 },
+        size: { x: 0, y: 0 },
+        rotation: 0,
+      }) as Sprite;
+
+      const snakeBody: SnakeBodyType[] = [
+        { position: { x: 0, y: 0 }, direction: { x: 0, y: 0 }, key: BodyType.head, sprite },
+        { position: { x: 0, y: 1 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 0, y: 2 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 1, y: 2 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 1, y: 1 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 1, y: 0 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 2, y: 0 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 2, y: 1 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+        { position: { x: 2, y: 2 }, direction: { x: 0, y: 0 }, key: BodyType.body, sprite },
+      ];
+
+      const result = IsSectionAvailable(snakeBody, { x: 0, y: 0 }, 9, 9, 3);
+      expect(result).toBeTruthy();
+
+    });
+
+
+  });
+
 })
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,50 @@
 import { Vec2 } from "palco-2d/types";
+import { SnakeBodyType } from "../types";
+
+export const IsSectionAvailable = (snakeBody: SnakeBodyType[], sections: Vec2, rows: number, cols: number, division: number) => {
+  const sectionXSize = rows / division;
+  const sectionYSize = cols / division;
+
+  for (let x = 0; x < sectionXSize; x++) {
+    for (let y = 0; y < sectionYSize; y++) {
+      const positionX = x + (sectionXSize * sections.x);
+      const positionY = y + (sectionYSize * sections.y);
+      console.log({ positionX, positionY });
+
+      const isFree = snakeBody.find(({ position }) => position.x === positionX && position.y === positionY);
+      console.log({ isFree });
+      if (isFree) return true;
+    }
+  }
+
+  return false;
+}
+
+export const getRandomSection = (snakeSection: Vec2, division: number) => {
+  const direction = {
+    x: snakeSection.x < division / 2 ? 1 : -1,
+    y: snakeSection.y < division / 2 ? 1 : -1
+  }
+
+  const max = division - 1;
+  const minX = snakeSection.x + direction.x;
+  const minY = snakeSection.y + direction.y;
+
+  const x = Math.random() * ((direction.x > 0 ? max : 0) - minX) + minX;
+  const y = Math.random() * ((direction.y > 0 ? max : 0) - minY) + minY;
+
+  return {
+    x: Math.round(x),
+    y: Math.round(y)
+  };
+}
+
+export const snakeReadSection = (snakeHeadPosition: Vec2, division: number) => {
+  return {
+    x: Math.ceil(snakeHeadPosition.x / division),
+    y: Math.ceil(snakeHeadPosition.y / division)
+  }
+}
 
 export const createSections = (rows: number, cols: number) => {
   const limitsRow = rows / 3;
